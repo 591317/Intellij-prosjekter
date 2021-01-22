48d0e1174bb120c7e00e818e9d438d9fa72be91a
591317@stud.hvl.no <591317@stud.hvl.no>
2021-01-22 21:13:23 +0100

    testing

---
+++ src/test.java
@@ -0,0 +1,23 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello test");
+    }
+
+    void method() {
+
+    }
+
+    void method1() {
+
+    }
+
+    void method2() {
+
+    }
+
+    void method3() {
+
+    }
+}
